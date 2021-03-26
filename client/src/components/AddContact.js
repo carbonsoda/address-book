470333297c5ce859ee2e89c97c4de342d2bf9fa4
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-26 11:31:34 -0700

    Add input fields + related hooks for AddContact

--- client/src/components/AddContact.js
+++ client/src/components/AddContact.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default function AddContact({ addContact }) {
+    const [first_name, setFirstName] = React.useState('');
+    const [last_name, setLastName] = React.useState('');
+    const [phone_number, setPhone] = React.useState('');
+    const [email, setEmail] = React.useState('');
 
     const submitForm = async (e) => {
         e.preventDefault();
@@ -11,6 +15,33 @@ export default function AddContact({ addContact }) {
   return (
       <>
           <form onSubmit={ submitForm }>
+              <label>First Name</label>
+              <input type="text"
+                  placeholder="John"
+                  value={ first_name }
+                  onChange={ e => setFirstName(e.target.value) }
+              />
+
+              <label>Last Name</label>
+              <input type="text"
+                  placeholder="Smith"
+                  value={ last_name }
+                  onChange={ e => setLastName(e.target.value) }
+              />
+
+              <label>Phone Name</label>
+              <input type="text"
+                  placeholder="123-456-7890"
+                  value={ phone_number }
+                  onChange={ e => setPhone(e.target.value) }
+              />
+
+              <label>Email Address</label>
+              <input type="text"
+                  placeholder="joe@email.com"
+                  value={ email }
+                  onChange={ e => setEmail(e.target.value) }
+              />
               
               <button type="submit">Submit</button>
 
