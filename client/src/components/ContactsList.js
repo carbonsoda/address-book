0fe67084a544a88c28ed0d9bbbb4db64add8d935
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-26 12:14:20 -0700

    Add var include/exclude delete btn for reusability

--- client/src/components/ContactsList.js
+++ client/src/components/ContactsList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Contact from './Contact';
 
-export default function ContactsList({ allContacts, deleteContact }) {
+export default function ContactsList({ allContacts, deleteContact, deleteEnabled }) {
 
     const deleteBtn = (id) => {
         deleteContact(id);
@@ -9,16 +9,17 @@ export default function ContactsList({ allContacts, deleteContact }) {
 
     return (
         <>
-            <h2>Contacts List</h2>
             <ul>
                 {
                     allContacts.map((contact) => (
-                        <li key={contact.id}>
+                        <li key={ contact.id }>
                             <Contact contact={ contact } />
-                            <button
-                                onClick={ () => deleteBtn(contact.id)}>
-                                Delete
+                            { !deleteEnabled ? ''
+                                : <button
+                                    onClick={ () => deleteBtn(contact.id) }>
+                                    Delete
                             </button>
+                            }
                         </li>
                     ))
                 }
