10a6c7a040e9e0b1dd65b4c8185960d3f8e9d7b6
Avery <22334165+carbonsoda@users.noreply.github.com>
2021-03-26 15:15:34 -0700

    Add datagrid to contactlist

--- client/src/components/ContactsList.js
+++ client/src/components/ContactsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Contact from './Contact';
+import { DataGrid } from '@material-ui/data-grid';
 
 export default function ContactsList({ allContacts, deleteContact, deleteEnabled }) {
 
@@ -7,23 +8,43 @@ export default function ContactsList({ allContacts, deleteContact, deleteEnabled
         deleteContact(id);
     }
 
+    const columns = [
+        { field: 'first_name', headerName: 'First Name', flex: .5 },
+        { field: 'last_name', headerName: 'Last Name', flex: .5 },
+        { field: 'phone_number', headerName: 'Phone', flex: .5 },
+        { field: 'email', headerName: 'Email', flex: .5 },
+        // { field: 'deleteBtn', headerName: 'D', flex: .5 }
+    ]
+
+    const rows = allContacts.map(contact => ({
+        id: contact.id,
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        phone_number: contact.phone_number,
+        email: contact.email
+    }))
+
     return (
         <>
-            <ul>
-                {
-                    allContacts.map((contact) => (
-                        <li key={ contact.id }>
-                            <Contact contact={ contact } />
-                            { !deleteEnabled ? ''
-                                : <button
-                                    onClick={ () => deleteBtn(contact.id) }>
-                                    Delete
-                            </button>
-                            }
-                        </li>
-                    ))
-                }
-            </ul>
+            <DataGrid
+                rows={ rows } columns={ columns }
+                pageSize={ 10 } autoHeight
+            />
         </>
     )
 }
+
+
+// {
+//     allContacts.map((contact) => (
+//         <li key={ contact.id }>
+//             <Contact contact={ contact } />
+//             { !deleteEnabled ? ''
+//                 : <button
+//                     onClick={ () => deleteBtn(contact.id) }>
+//                     Delete
+//                             </button>
+//             }
+//         </li>
+//     ))
+// }
